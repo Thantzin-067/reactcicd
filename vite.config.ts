0347187afd20cabee10435d63d592e21ca156a8d
thantzin <thantzin7826@gmail.com>
2026-06-16 13:59:17 +0630

    deploy on github page1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/reactcicd/', // သင့် repository နာမည်ကို ထည့်ပေးပါ
   plugins: [react()],
 })
